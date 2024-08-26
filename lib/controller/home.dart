140d5ab633c9ac9ec9c610fba264e578afcd588b
yacine37th <y.titouah@esi-sba.dz>
2024-08-26 22:40:24 +0100

    home page updated

--- lib/controller/home.dart
+++ lib/controller/home.dart
@@ -1,6 +1,36 @@
 import 'package:get/get.dart';
 
+import '../model/event.dart';
+
 class HomeController extends GetxController {
-
-
+  final List<EventModel> flutterTopicsList = [
+    EventModel(
+      id: "0",
+      topicIcon: "assets/images/houses.png",
+      topicName: "كراء المنازل",
+      link: '/Istichara',
+      conditions: '',
+    ),
+    EventModel(
+      id: "1",
+      topicIcon: "assets/images/businessman.png",
+      topicName: "الوكالات السياحية",
+      link: '/IwaaStudent',
+      conditions: '',
+    ),
+    EventModel(
+      id: "2",
+      topicIcon: "assets/images/guide.png",
+      topicName: "المرشد السياحي",
+      link: '/ElectronicLibrary',
+      conditions: '',
+    ),
+    EventModel(
+      id: "3",
+      topicIcon: "assets/images/trips.png",
+      topicName: "رحلات التخييم",
+      link: '/Dawarat',
+      conditions: '',
+    ),
+  ];
 }
